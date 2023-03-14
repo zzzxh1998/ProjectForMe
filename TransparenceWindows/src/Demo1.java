7c4cd45544f968039154df2ef6a9127c9db8bead
zhangxiaohu <zhangxiaohu@nti56.com>
2023-03-14 14:01:10 +0800

    透明窗口1-Demo1

--- TransparenceWindows/src/Demo1.java
+++ TransparenceWindows/src/Demo1.java
@@ -1,28 +1,8 @@
 import javax.swing.*;
 
-public class TransparentExample {
-    public TransparentExample() {
+import static javax.swing.text.StyleConstants.setBackground;
 
-        super("TransparentExample");
+public class Demo1 {
 
-        JPanel panel = new JPanel();
-        panel.add(new JButton("Button"));
-
-        setContentPane(panel);
-        setBackground(new Color(0, 0, 0, 0));
-        setSize(500, 500);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-
-    public static void main(String[] args) {
-
-        JFrame.setDefaultLookAndFeelDecorated(true);
-
-        TransparentExample frame = new TransparentExample();
-        frame.setVisible(true);
-
-    }
 
 }
